1ca06b3ccdc8bb2dc9f7ed3ea06fbf0fe3948b1e
jack <jack@git.com>
2023-12-08 12:08:01 +0530

    Added Multi Stack and modified stack using array to add new operations

--- stack-using-array.c
+++ stack-using-array.c
@@ -2,20 +2,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define max 10
 
 void push(int x);
 int pop();
 void display();
-int stack[10],top = -1;
+int isEmpty();
+int isFull();
+int len();
+int topItem();
+
+int stack[max];
+int top = -1;
 
 int main(){
     int op,x;
     while(1){
-        printf("1.Push\n2.Pop\n3.Display\n4.Exit\nEnter Your choice: ");
+        printf("1.Push, 2.Pop, 3.Display, 4. Length, 5. isEmpty, 6. isFull, 7. Top, 8.Exit.\nEnter Your choice: ");
         scanf("%d",&op);
         switch(op){
             case 1:
-                if(top == 9){
+                if(isFull()){
                     printf("Overflow.\n");
                 }else{
                     printf("Enter the item: ");
@@ -24,15 +31,15 @@ int main(){
                 }
                 break;
             case 2:
-                if(top == -1){
+                if(isEmpty()){
                     printf("Underflow.\n");
                 }else{
                     x = pop();
-                    printf("Removed %d from stack",x);
+                    printf("Removed %d from stack\n",x);
                 }
                 break;
             case 3:
-                if(top == -1){
+                if(isEmpty()){
                     printf("Stack is empty.\n");
                 }else{
                     printf("The elements of stack are: ");
@@ -40,6 +47,30 @@ int main(){
                 }
                 break;
             case 4:
+                printf("%d\n",len());
+                break;
+            case 5:
+                if(isEmpty()){
+                    printf("Stack is Empty\n");
+                }else{
+                    printf("Stack is Not Empty\n");
+                }
+                break;
+            case 6:
+                if(isFull()){
+                    printf("Stack is Full\n");
+                }else{
+                    printf("Stack is Not Full\n");
+                }
+                break;
+            case 7:
+                if(isEmpty()){
+                    printf("Stack is Empty\n");
+                }else{
+                    printf("%d\n",topItem());
+                }
+                break;
+            case 8:
                 exit(0);
             default:
                 printf("Invalid Input.\n");
@@ -61,4 +92,26 @@ void display(){
         printf("%d -> ",stack[i]);
     }
     printf("%d\n",stack[top]);
+}
+
+int isEmpty(){
+    if(top == -1){
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(){
+    if(top == max-1){
+        return 1;
+    }
+    return 0;
+}
+
+int len(){
+    return top+1;
+}
+
+int topItem(){
+    return stack[top];
 }
