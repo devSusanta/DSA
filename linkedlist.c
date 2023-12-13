3fb3f83b1a522d5e707a508055b5ff9d341ca708
jack <jack@git.com>
2023-12-13 20:58:39 +0530

    Added for testing

--- linkedlist.c
+++ linkedlist.c
@@ -1,14 +1,18 @@
 #include "linkedlist.h"
 
 int main(){
-    lkdlist *st1 = NULL;
+    lkdlist *st1 = NULL, *st2 = NULL;
     int n = 20;
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i < n; i+=2){
         push_top(&st1,i);
     }
+    for(int i = 0; i < n; i+=2){
+        push_top(&st2,i);
+    }
+    lkdlist *st3 = merge(NULL,st2);
     display(st1);
-    sort_list(&st1);
-    display(st1);
+    display(st2);
+    display(st3);
     if(isExist(st1,10)){
         printf("Element Found.");
     }else{
