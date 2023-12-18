fd4056529215f11134ac575335fc350af01baef3
jack <jack@git.com>
2023-12-18 22:12:54 +0530

    Added New Methods

--- cdlinkedlist.c
+++ cdlinkedlist.c
@@ -2,31 +2,28 @@
 
 void main(){
     cdl_list *ll1 = NULL;
+    sort_push(&ll1, 8);
     push_top(&ll1,10);
     push_top(&ll1,12);
     push_top(&ll1,13);
     push_bottom(&ll1,100);
     push_bottom(&ll1,100);
     push_bottom(&ll1,100);
-    push(&ll1,100,0);
     push(&ll1,300,len(ll1));
     push_top(&ll1,15);
     push(&ll1,123,8);
     printf("The len of this list is %d\n",len(ll1));
     display(ll1);
-    pop_top(&ll1);
     display(ll1);
-    pop_bottom(&ll1);
     display(ll1);
-    pop_at(&ll1,2);
     display(ll1);
-    for(int i = 0; i < 301; i++){
-        if(isExist(ll1, i)){
-            printf("%d is Exist in this List.\n",i);
-        }
-    }
     printf("%d\n",count(ll1, 10));
     printf("index %d is %d\n",0,item_at(ll1,6));
     printf("The top and bottom of this list is %d, %d\n",top(ll1),bottom(ll1));
+    sort_list(&ll1);
     display(ll1);
+    cdl_list *ll2 = sort_merge(ll1,ll1);
+    display(ll2);
+    replaceAll(ll2,100,12);
+    display(ll2);
 }
--- cdlinkedlist.h
+++ cdlinkedlist.h
@@ -129,6 +129,28 @@ void push_bottom(cdl_list **head, int new_data){
     push(head, new_data, len(*head));
 }
 
+void sort_push(cdl_list **head, int new_data){
+    if(!*head || (*head)->data > new_data){
+        push(head, new_data, 0);
+        return;
+    }
+    cdl_list *new_node = (cdl_list*)malloc(sizeof(cdl_list));
+    new_node->data = new_data;
+    cdl_list *temp = *head, *prev;
+    while(temp->data < new_data && temp->next != *head){
+        prev = temp;
+        temp = temp->next;
+    }
+    if(temp->data <= new_data){
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = new_node;
+    new_node->prev = prev;
+    temp->prev = new_node;
+    new_node->next = temp;
+}
+
 int pop_at(cdl_list **head, int index){
     int l = len(*head);
     if(l <= index || index < 0){
@@ -175,6 +197,34 @@ int pop_top(cdl_list **head){
 }
 int pop_bottom(cdl_list **head){
     return pop_at(head, len(*head)-1);
+}
+
+void pop(cdl_list **head, int key){
+    if(!*head){
+        printf("List is Empty.(No Element Found)\n");
+        return;
+    }
+    if((*head)->data == key){
+        pop_top(head);
+    }
+    cdl_list *temp = *head, *prev;
+    while(temp->next != *head && temp->data != key){
+        prev = temp;
+        temp = temp->next;
+    }
+    if(temp->next == *head){
+        if(temp->data == key){
+            prev->next = temp->next;
+            temp->next->prev = prev;
+            free(temp);
+        }else{
+            printf("element not found.\n");
+        }
+        return;
+    }
+    prev->next = temp->next;
+    temp->next->prev = prev;
+    free(temp);
 }
 
 // int pop_top(cdl_list **head){
@@ -276,4 +326,139 @@ void displayr(cdl_list *head){
         temp = temp->prev;
     }
     printf("%d\n",temp->data);
+}
+
+void sort_list(cdl_list **head){
+    if(!*head){
+        return;
+    }
+    cdl_list *temp = *head;
+    int isSorted = 1;
+    int x = 0;
+    while(isSorted){
+        isSorted = 1;
+        while(temp->next != *head){
+            if(temp->data > temp->next->data){
+                x = temp->data;
+                temp->data = temp->next->data;
+                temp->next->data = x;
+                isSorted++;
+            }
+            temp = temp->next;
+        }
+        isSorted--;
+        temp = *head;
+    }
+}
+
+void reverse_list(cdl_list **head){
+    cdl_list *temp = NULL;
+    cdl_list *temp2 = *head;
+    do{
+    // printf("Hii ");
+        temp = (*head)->next;
+        (*head)->next = (*head)->prev;
+        (*head)->prev = temp;
+        (*head) = (*head)->prev;
+    }while((*head)->next != temp2);
+
+    // (*head) = (*head)->next;
+    temp = (*head)->next;
+    (*head)->next = (*head)->prev;
+    (*head)->prev = temp;
+}
+
+cdl_list *merge(cdl_list *head1, cdl_list *head2){
+    cdl_list *head = NULL;
+    cdl_list *temp1 = head1;
+    cdl_list *temp2 = head2;
+    if(head1){
+        while(head1->next != temp1){
+            push_bottom(&head, head1->data);
+            head1 = head1->next;
+        }
+        push_bottom(&head, head1->data);
+    }
+    if(head2){
+        while(head2->next != temp2){
+            push_bottom(&head,head2->data);
+            head2 = head2->next;
+        }
+        push_bottom(&head, head2->data);
+    }
+    return head;
+}
+
+cdl_list *sort_merge(cdl_list *head1, cdl_list *head2){
+    cdl_list *head = NULL;
+    cdl_list *temp1 = head1;
+    cdl_list *temp2 = head2;
+    if(head1){
+        while(head1->next != temp1){
+            sort_push(&head, head1->data);
+            head1 = head1->next;
+        }
+        sort_push(&head, head1->data);
+    }
+    if(head2){
+        while(head2->next != temp2){
+            sort_push(&head,head2->data);
+            head2 = head2->next;
+        }
+        sort_push(&head, head2->data);
+    }
+    return head;
+}
+
+
+void replace(cdl_list *head, int old_data, int new_data){
+    if(!head){
+        printf("No Element Found to Replace.\n");
+        return;
+    }
+    cdl_list *temp = head;
+    while(head->next != temp){
+        if(head->data == old_data){
+            head->data = new_data;
+            return;
+        }
+        head = head->next;
+    }
+    printf("No Element Found to Replace.\n");
+}
+
+void replace_at(cdl_list *head, int index, int new_data){
+    if(!head){
+        printf("No Element Found to Replace.\n");
+        return;
+    }
+
+    int i = 0;
+    cdl_list *temp = head;
+    while(head->next != temp){
+        if(i == index){
+            head->data = new_data;
+            return;
+        }
+        i++;
+        head = head->next;
+    }
+    printf("No Element Found to Replace.\n");
+}
+
+void replaceAll(cdl_list *head, int old_data, int new_data){
+    if(!head){
+        printf("No Element Found to Replace.\n");
+        return;
+    }
+    int placed = 0;
+    cdl_list *temp = head;
+    while(head->next != temp){
+        if(head->data == old_data){
+            head->data = new_data;
+            placed++;
+        }
+        head = head->next;
+    }
+    if(!placed)printf("No Element Found to Replace.\n");
 }
