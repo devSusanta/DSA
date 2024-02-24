96ade092955b38e165cfae9acbd365a698ccd112
jack <jack@jack.com>
2024-02-24 13:05:56 +0530

    added new function to the merge sort method

--- Practicals/1.merge-two-sorted-array.cpp
+++ Practicals/1.merge-two-sorted-array.cpp
@@ -3,7 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sort_merge(int arr[], int arr1[], int arr2[],int l1, int l2){
+void merge(int arr[], int arr1[], int arr2[],int l1, int l2){
     int l = l1+l2;
     int i = 0;
     int j = 0;
@@ -36,13 +36,34 @@ void sort_merge(int arr[], int arr1[], int arr2[],int l1, int l2){
     }
 }
 
+//this is the little version of the merge funciton both uses the same concept to merge two sorted array.
+
+// void merge(int arr[], int arr1[], int arr2[], int l1, int l2) {
+//     int i = 0, j = 0, k = 0;
+
+//     while (i < l1 + l2) {
+//         if (j >= l1) {
+//             // arr1 is exhausted, copy remaining elements from arr2
+//             arr[i++] = arr2[k++];
+//         } else if (k >= l2) {
+//             // arr2 is exhausted, copy remaining elements from arr1
+//             arr[i++] = arr1[j++];
+//         } else if (arr1[j] <= arr2[k]) {  // Handle potential duplicates
+//             arr[i++] = arr1[j++];
+//         } else {
+//             arr[i++] = arr2[k++];
+//         }
+//     }
+// }
+
+
 int main(){
     int arr[10];
     int arr1[] = {1,3,5,9,10};
     int arr2[] = {2,3,4,6,8};
     int l1 = sizeof(arr1)/sizeof(arr1[0]);
     int l2 = sizeof(arr2)/sizeof(arr2[0]);
-    sort_merge(arr,arr1,arr2,l1,l2);
+    merge(arr,arr1,arr2,l1,l2);
     int i = 0;
     while(i < 10){
         cout << arr[i] << " ";
